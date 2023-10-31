cdf84b3297a9c68381a4719ed31fd6aefda11dd3
chrstnbgt <bagotaochristiana@gmail.com>
2023-10-31 23:23:52 +0800

    Update editform.php

--- forms/editform.php
+++ forms/editform.php
@@ -1,10 +1,10 @@
 <?php
     include_once("../dbConnection/mysqlconfig_connection.php");
     $id = $_GET['id'];
-    $result = mysqli_query($dbc, "SELECT * FROM tblsubject WHERE Subject_ID=$id");
+    $result = mysqli_query($dbc, "SELECT * FROM tblsubjects WHERE Subject_ID=$id");
     while($res = mysqli_fetch_array($result)) {
-        $code = $res['subject_code'];
-        $name = $res['subject_name'];
+        $code = $res['Subject_Code'];
+        $name = $res['Subject_Name'];
     }
 
 ?>
@@ -19,7 +19,7 @@
         <a href="../index.php">Home</a>
         <br/><br/>
         <form name="form1" method="post" action="../functions/edit.php">
-            <table border="0">
+            <table-border="0">
                 <tr>
                     <td>Subject Code</td>
                     <td><input type="text" name="code" value="<?php echo $code;?>"></td>
@@ -29,8 +29,8 @@
                     <td><input type="text" name="name" value="<?php echo $name;?>"></td>
                 </tr>
                 <tr>
-                    <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?></td>
-                    <td><input type="submit" name="Submit" value="Add"></td>
+                    <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+                    <td><input type="submit" name="Submit" value="Edit"></td>
                 </tr>
             </table>
         </form>
