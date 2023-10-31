5f710ce8fd5e11438c4cf9b195011be1b94210d2
chrstnbgt <bagotaochristiana@gmail.com>
2023-10-31 20:40:54 +0800

    forms

---
+++ forms/editform.php
@@ -0,0 +1,39 @@
+<?php
+    include_once("../dbConnection/mysqlconfig_connection.php");
+    $id = $_GET['id'];
+    $result = mysqli_query($dbc, "SELECT * FROM tblsubject WHERE Subject_ID=$id");
+    while($res = mysqli_fetch_array($result)) {
+        $code = $res['subject_code'];
+        $name = $res['subject_name'];
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Edit Data</title>
+    </head>
+    <body>
+        <h1>Edit Subject</h1>
+        <a href="../index.php">Home</a>
+        <br/><br/>
+        <form name="form1" method="post" action="../functions/edit.php">
+            <table border="0">
+                <tr>
+                    <td>Subject Code</td>
+                    <td><input type="text" name="code" value="<?php echo $code;?>"></td>
+                </tr>
+                <tr>
+                    <td>Subject Name</td>
+                    <td><input type="text" name="name" value="<?php echo $name;?>"></td>
+                </tr>
+                <tr>
+                    <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?></td>
+                    <td><input type="submit" name="Submit" value="Add"></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
