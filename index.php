bb6695ae7024f44d7e39ca78cb7b62c8694a30bf
chrstnbgt <bagotaochristiana@gmail.com>
2023-10-31 23:23:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,26 +10,27 @@
     <title>Sample CRUD</title>
 </head>
 <body>
-    <h1>My Subjects</h1>
-    <a href="form/addform.php">Add Subject</a><br></br>
-    <table width = '100%' border = 1>
-        <tr bgcolor= '#CCCCCC'>
-            <td>ID</td>
-            <td>Subject Code</td>
-            <td>Subject Name</td>
-            <td>Action</td>
-        </tr>
+    <h1 style="text-align: center;">MY SUBJECTS</h1>
+    <table width = '100%' border=1.5>
+    <tr style="text-align: center;" bgcolor='#CCCCCC' color='#000000'>
+        <td style="font-weight: bold;">ID</td>
+        <td style="font-weight: bold;">Subject Code</td>
+        <td style="font-weight: bold;">Subject Name</td>
+        <td style="font-weight: bold;" width="15%">Action</td>
+    </tr>
+
         <?php
             while($res = mysqli_fetch_array($result))   {
                 echo "<tr>";
-                echo "<td>".$res['Subject_ID']."</tr>";
-                echo "<td>".$res['Subject_Code']."</tr>";
-                echo "<td>".$res['Subject_Name']."</tr>";
-                echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a> |
-                        <a href=\"functions/delete.php?id=$res[Subject_ID]\"
-                        onClick=\"return confirm('Are you sure you want to delete?'\"Delete</a></td>";
+                echo "<td>".$res['Subject_ID']."</td>";
+                echo "<td>".$res['Subject_Code']."</td>";
+                echo "<td>".$res['Subject_Name']."</td>";
+                echo "<td><a href=\"forms/editform.php?id=".$res['Subject_ID']."\">Edit</a> |
+                        <a href=\"functions/delete.php?id=".$res['Subject_ID']."\"
+                        onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
             }
         ?>
-    </table>
+    </table><br/>
+    <a href="forms/addform.php">ADD SUBJECT</a>
 </body>
 </html>
