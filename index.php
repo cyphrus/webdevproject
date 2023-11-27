2bf11eeb260f333710123587729c5c2d213d8504
cyphrus <rafaelcastelo2003@gmail.com>
2023-11-27 23:27:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
         <td style="font-weight: bold;">ID</td>
         <td style="font-weight: bold;">Subject Code</td>
         <td style="font-weight: bold;">Subject Name</td>
+        <td style="font-weight: bold;">Syllabus ID</td>
+        <td style="font-weight: bold;">Syllabus Code</td>
+        <td style="font-weight: bold;">Syllabus Author</td>
         <td style="font-weight: bold;" width="15%">Action</td>
     </tr>
 
@@ -25,6 +28,9 @@
                 echo "<td>".$res['Subject_ID']."</td>";
                 echo "<td>".$res['Subject_Code']."</td>";
                 echo "<td>".$res['Subject_Name']."</td>";
+                echo "<td>".$res['syllabus_id']."</td>";
+                echo "<td>".$res['syllabus_code']."</td>";
+                echo "<td>".$res['syllabus_author']."</td>";
                 echo "<td><a href=\"forms/editform.php?id=".$res['Subject_ID']."\">Edit</a> |
                         <a href=\"functions/delete.php?id=".$res['Subject_ID']."\"
                         onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
