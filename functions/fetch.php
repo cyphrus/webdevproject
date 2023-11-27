d7f7efd330a8b84974cdf6aead2e6cca38bf464c
cyphrus <rafaelcastelo2003@gmail.com>
2023-11-28 00:40:48 +0800

    Update fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,4 +1,6 @@
 <?php
-    $query = "<<UPDATE SELECT QUERY>>";
+    $query = "SELECT * FROM tblsubjects
+              LEFT JOIN tblsyllabus
+              ON tblsyllabus.Subject_ID = tblsubjects.Subject_ID";
     $result = mysqli_query($dbc, $query);
-?>
+?>  
