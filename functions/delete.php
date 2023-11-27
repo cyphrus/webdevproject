1d0d5f1a34e27eb826c5a0e986bff82c4752d925
chrstnbgt <bagotaochristiana@gmail.com>
2023-11-28 01:46:55 +0800

    Update delete.php

--- functions/delete.php
+++ functions/delete.php
@@ -1,6 +1,7 @@
 <?php
     include_once("../dbconnection/mysqlconfig_connection.php");
     $id = $_GET['id'];
-    mysqli_query($dbc, "DELETE FROM tblsubjects WHERE subject_id = '$id'");
+    mysqli_query($dbc, "DELETE FROM tblsyllabus WHERE Subject_ID = '$id'");
+    mysqli_query($dbc, "DELETE FROM tblsubjects WHERE Subject_ID = '$id'");
     header("Location:../index.php");
 ?>
