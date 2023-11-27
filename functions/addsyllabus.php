b3633b0d04d31ebc2416176fee9571db0d2afef3
chrstnbgt <bagotaochristiana@gmail.com>
2023-11-28 00:58:32 +0800

    syllabus

---
+++ functions/addsyllabus.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+
+        include_once("../dbConnection/mysqlconfig_connncetion.php");
+
+        if(isset($_POST['Submit'])){
+            $code = $_POST['code'];
+            $author = $_POST['author'];
+            $subject = $_POST['subject'];
+            
+            if(empty($code) || empty($author)){
+                if(empty($code)){
+                    echo "<font color='red'>Syllabus code field is empty</font><br/>"; 
+                }
+                if(empty($author)){
+                    echo "<font color='red'>Syllabus author field is empty</font><br/>"; 
+                }
+                echo "<br/><a href ='javascript:self:history.back();'>Go Back</a>"
+            }
+            else {
+                $result = mysqli_query($dbc, "INSERT INTO tblsyllabus(syllabus_code, syllabus_author, Subject_ID) VALUES('$code', '$author', '$subject')");
+                
+                echo "<font color='green'>Data added successfully.</font>";
+                echo "<br/><a href ='../index.php>View Result</a>'";
+            }
+        }
+
+        ?>
+    
+</body>
+</html>
